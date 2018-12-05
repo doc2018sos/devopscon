2019a75fa4e3d0bf0bf94b6e2091882001dbec76
doc2018sos <45556151+doc2018sos@users.noreply.github.com>
2018-12-05 10:01:56 +0100

    change message

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello DevOpsCon members, What do you think about this conference?";
+		String hello = "Great stuff out here :-)";
 		return hello;
 	}
 
